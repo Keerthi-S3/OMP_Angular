89f8d6bdf4c4ead83ebf8e93215c40a6a6073d5c
Keerthi <keerthi.s3@cognizant.com>
2025-04-15 22:15:55 +0530

    second commit

--- Angular_App/src/app/product-details/product-details.component.ts
+++ Angular_App/src/app/product-details/product-details.component.ts
@@ -13,31 +13,31 @@ import { UserReviewComponent } from "../user-review/user-review.component";
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
-[x: string]: any;
-
-  
-public product: any;
-
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  [x: string]: any;
 
 
-ngOnInit() {
-      const productId = this.route.snapshot.paramMap.get('id');
-      if (productId) {
-        this.http.get(`http://localhost:8090/OMP/viewProductDetails/${productId}`).subscribe(
-          (product: any) => {
-            this.product = product;
-          },
-          error => {
+  public product: any;
 
-    this.product = null;
-          }
-        );
-      } else {
-        this.product = null;
-      }
-    }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+
+  ngOnInit() {
+    const productId = this.route.snapshot.paramMap.get('id');
+    if (productId) {
+      this.http.get(`http://localhost:8090/OMP/viewProductDetails/${productId}`).subscribe(
+        (product: any) => {
+          this.product = product;
+        },
+        error => {
+
+          this.product = null;
+        }
+      );
+    } else {
+      this.product = null;
+    }
   }
-    
+}
+
 
 
